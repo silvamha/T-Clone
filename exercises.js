f7a40fd2ab933a932d28453459975b3cc73baf7a
Ed <silva.email@yahoo.com>
2024-10-09 17:16:13 -0500

    exercises

---
+++ exercises.js
@@ -0,0 +1,76 @@
+import { tweetsData } from "./data.js";
+
+const testDestructuring = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY',
+        zip: '10001'
+    }
+}
+
+const { firstName, lastName, age, address: { city } } = testDestructuring
+
+console.log(firstName, lastName, age, city)
+
+for (let test in testDestructuring) {
+    console.log(firstName, lastName, age, city)
+}
+
+
+// Help me create a .forEach() 
+
+
+// Original Array
+const numbers = [1, 2, 3, 4, 5];
+
+const newArrayForEach = []
+
+const newArrayMap =[]
+
+// .forEach() method
+numbers.forEach((num =>{
+    // Populate new array
+return newArrayForEach.push(num)
+}))
+
+// Log new array using .forEach()
+console.log(newArrayForEach)
+
+// .map() method
+newArrayForEach.map((n =>{
+    // How is this different than .forEach()?
+    // Populate new array
+    return newArrayMap.push(n)
+}))
+
+// Log new array using .map()
+console.log(newArrayMap)
+
+const fruits = ['apple', 'banana', 'orange'];
+
+const newFruits =[]
+
+fruits.forEach((fruit =>{
+
+return newFruits.push(fruit)
+
+}))
+
+
+console.log(newFruits)
+
+console.log(tweetsData)
+
+tweetsData.forEach((tweet =>{
+    console.log(tweet.replies.length)
+    if(tweet.replies.length >0){
+        tweet.replies.forEach((reply) =>{
+            console.log(`${reply.handle} ${reply.profilePic} ${reply.tweetText}`)
+        })
+    }
+}))
+
--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 import { tweetsData } from "./data.js";
 
-for (let tweet of tweetsData) {
-  for (let tweetObeject in tweet) {
-    console.log(tweetObeject);
-  }
-  console.log(tweet);
-}
+// for (let tweet of tweetsData) {
+//   for (let tweetObeject in tweet) {
+//     console.log(tweetObeject);
+//   }
+//   console.log(tweet);
+// }
+
+
+const grades = [75, 83, 66, 43, 55, 99, 87, 16, 89, 64, 70, 80, 94, 77, 66, 73]
+
+const totalGrades = grades.reduce((total, currentGrades) =>{
+    return total + currentGrades
+})
+
+
+console.log(`${Math.round(totalGrades/grades.length)}`)
+
+const studentsArr = [
+    {
+        name: 'Mike',
+        grade: 75
+    },
+    {
+        name: 'Emma',
+        grade: 83
+    },
+    {
+        name: 'Seth',
+        grade: 66
+    }
+]
+
+
